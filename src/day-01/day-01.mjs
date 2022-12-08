f5e7860afdcb0da8b3553771891040016c66accb
Cody Swartz <ctsstc@gmail.com>
2022-12-07 23:51:50 -0800

    Solve day 1 part 1

---
+++ src/day-01/day-01.mjs
@@ -0,0 +1,51 @@
+import Problem from '../_lib/problem.mjs'
+
+class Day01 extends Problem {
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  solvePart1() {
+    let currentElfIndex = 0
+
+    this.linesAsInts
+
+    const elves = [0]
+    for (let i = 0; i < this.linesAsInts.length; i++) {
+      const currentValue = this.linesAsInts[i]
+
+      if (currentValue === 0) {
+        currentElfIndex++
+        elves.push(0)
+      }
+      else {
+        elves[currentElfIndex] += currentValue
+      }
+    }
+
+    const maxValue = Math.max(...elves)
+
+    return maxValue
+
+    // this.linesAsInts.reduce((collection, currentValue) => {
+    //   const currentElfValue = collection[currentElfIndex]
+
+
+
+    //   if (currentElfValue === undefined) {
+    //     collection.push(currentValue)
+    //   }
+    //   else {
+    //     collection[currentElfIndex] = currentElfValue + currentValue
+    //   }
+
+    //   return collection
+    // }, [])
+  }
+
+  solvePart2() {
+    return parseInt(this.solvePart1())
+  }
+}
+
+export default Day01
---
+++ src/day-01/day-01.test.mjs
@@ -0,0 +1,32 @@
+import Problem from './day-01.mjs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+describe('Day 01', () => {
+  describe('example.txt', () => {
+    const problem = new Problem(__dirname + '/example.txt')
+
+    it('solves part 1', () => {
+      expect(problem.solvePart1()).toBe(24000)
+    })
+
+    // it('solves part 2', () => {
+    //   expect(problem.solvePart2()).toBe(123)
+    // })
+  })
+
+  describe('input.txt', () => {
+    const problem = new Problem(__dirname + '/input.txt')
+
+    it('solves part 1', () => {
+      expect(problem.solvePart1()).toBe(71023)
+    })
+
+    // it('solves part 2', () => {
+    //   expect(problem.solvePart2()).toBe(456789)
+    // })
+  })
+})
