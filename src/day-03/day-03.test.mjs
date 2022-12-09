124e61d8cd6bfdf902ac721b5c080ddb652618be
Lee Pham <leenpham@gmail.com>
2022-12-09 00:10:30 -0800

    UwU Senpaiiiiii

---
+++ src/day-03/day-03.test.mjs
@@ -0,0 +1,32 @@
+import Problem from './day-03.mjs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+describe('Day 03', () => {
+  describe('example.txt', () => {
+    const problem = new Problem(__dirname + '/example.txt')
+
+    xit('solves part 1', () => {
+      expect(problem.solvePart1()).toBe('1')
+    })
+
+    // it('solves part 2', () => {
+    //   expect(problem.solvePart2()).toBe(123)
+    // })
+  })
+
+  describe('input.txt', () => {
+    const problem = new Problem(__dirname + '/input.txt')
+
+    // it('solves part 1', () => {
+    //   expect(problem.solvePart1()).toBe('456789')
+    // })
+
+    // it('solves part 2', () => {
+    //   expect(problem.solvePart2()).toBe(456789)
+    // })
+  })
+})
