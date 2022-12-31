7624b3db360161478bb6b706aa6030257826703d
Lee Pham <leenpham@gmail.com>
2022-12-31 12:52:13 -0800

    Created day 08

---
+++ src/day-08/day-08.mjs
@@ -0,0 +1,17 @@
+import Problem from '../_lib/problem.mjs'
+
+class Day08 extends Problem {
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  solvePart1() {
+    return this.lines.reduce((sb, line) => sb + line, '')
+  }
+
+  solvePart2() {
+    return parseInt(this.solvePart1())
+  }
+}
+
+export default Day08
