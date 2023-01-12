39fcb50b138be93940d5a17a01cb8ac160b11838
Cody Swartz <ctsstc@gmail.com>
2023-01-11 23:00:18 -0800

    Solve day 10 part 2
    Problems needed to be reinstantiated to reset the state between tests.
    
    Co-authored-by: Lee Pham <LeeNPham@users.noreply.github.com>

--- src/day-10/day-10.mjs
+++ src/day-10/day-10.mjs
@@ -9,6 +9,7 @@ class Day10 extends Problem {
   #currentCycle = 0
   #currentValue = 1
   #sum = 0
+  #screen = []
 
   constructor(inputFileName) {
     super(inputFileName)
@@ -47,6 +48,69 @@ class Day10 extends Problem {
     return this.#sum
   }
 
+  solvePart2() {
+    this.lines.forEach((line) => {
+      // console.log({line})
+      const [operation, strVal] = line.split(' ')
+
+      if (operation == NOOP_COMMAND) {
+        this.#incrementCycleAndDraw()
+
+        this.#addSumIfSummableCycle()
+      }
+      else if (operation == ADDX_COMMAND) {
+        const value = parseInt(strVal)
+
+        for (let i = 0; i < ADDX_CYCLE_COUNT; i++) {
+          this.#incrementCycleAndDraw()
+
+          // Increase the current value on the second cycle
+          if (i == 1) {
+            this.#currentValue += value
+          }
+        }
+      }
+      else {
+        throw new Error("IMPOSSIBRUUUU!!!")
+      }
+    })
+
+    return this.#drawScreen()
+  }
+
+  #drawScreen() {
+    const lines = 6
+
+    let drawScreen = '\n'
+    for (let i = 0; i < lines; i++) {
+      const startIndex = i * 40
+      const endIndex = startIndex + 40
+      const currentLine = this.#screen.slice(startIndex, endIndex)
+      const drawLine = currentLine.map((line) => line == true ? '#' : '.').join('')
+      // console.log({currentLine, drawLine})
+      drawScreen += drawLine + "\n"
+    }
+
+    console.log("SCREENIE BOY 9000™")
+    console.log(drawScreen)
+
+    return drawScreen
+  }
+
+  #incrementCycleAndDraw() {
+    this.#screen.push(this.#isValueInCycleRange())
+    this.#currentCycle++
+  }
+
+  #isValueInCycleRange() {
+    const diff = Math.abs(this.#currentValue - (this.#currentCycle % 40))
+
+    // console.log({diff, value: this.#currentValue, cycle: this.#currentCycle })
+
+    // Can be in a range of 1 above or 1 below
+    return diff <= 1
+  }
+
   #addSumIfSummableCycle() {
     if (this.#isSummableCycle()) {
       // console.log({
@@ -71,8 +135,10 @@ class Day10 extends Problem {
     // })
     return this.#currentCycle * this.#currentValue
   }
- 
+
   /**
+   * === PROBLEM 1 ===
+   * 
    * Need to have an idea of a "cycle" counter, and current value / register
    * 
    * We have 2 operations: `addx` and `noop`
@@ -91,6 +157,22 @@ class Day10 extends Problem {
    * 
    * Sum/Answer:
    *  Every 20 + 40x cycles we need to add the current signal strength to a sum which will the final answer
+   * 
+   */
+
+  /**
+   * === PROBLEM 2 ===
+   * Store the screen in a single dimensional array of true/false values
+   * 
+   * Drawing:
+   *  take slices of the array by 40
+   *  map the slices from true/false to #/.
+   *  add a new line at the end of each 40
+   * 
+   * Determine if the pixel lights up for the current cycle:
+   *  Look at the current value and determine if the current cycle is within a range of 
+   *  -1 through +1 of the current value since the sprite is 3 pixels wide with the center
+   *  in the middle at pixel 2.
    * 
    */
 }
--- src/day-10/day-10.test.mjs
+++ src/day-10/day-10.test.mjs
@@ -7,26 +7,44 @@ const __dirname = dirname(__filename)
 
 describe('Day 10', () => {
   describe('example.txt', () => {
-    const problem = new Problem(__dirname + '/example.txt')
-
     it('solves part 1', () => {
+      const problem = new Problem(__dirname + '/example.txt')
       expect(problem.solvePart1()).toBe(13140)
     })
 
-    // it('solves part 2', () => {
-    //   expect(problem.solvePart2()).toBe(123)
-    // })
+    it('solves part 2', () => {
+      const problem = new Problem(__dirname + '/example.txt')
+      expect(problem.solvePart2()).toMatchInlineSnapshot(`
+        "
+        ##..##..##..##..##..##..##..##..##..##..
+        ###...###...###...###...###...###...###.
+        ####....####....####....####....####....
+        #####.....#####.....#####.....#####.....
+        ######......######......######......####
+        #######.......#######.......#######.....
+        "
+      `)
+    })
   })
 
   describe('input.txt', () => {
-    const problem = new Problem(__dirname + '/input.txt')
-
     it('solves part 1', () => {
+      const problem = new Problem(__dirname + '/input.txt')
       expect(problem.solvePart1()).toBe(14040)
     })
 
-    // it('solves part 2', () => {
-    //   expect(problem.solvePart2()).toBe(456789)
-    // })
+    it('solves part 2', () => {
+      const problem = new Problem(__dirname + '/input.txt')
+      expect(problem.solvePart2()).toMatchInlineSnapshot(`
+        "
+        ####..##...##....##.####...##.####.#....
+        ...#.#..#.#..#....#....#....#.#....#....
+        ..#..#....#.......#...#.....#.###..#....
+        .#...#.##.#.......#..#......#.#....#....
+        #....#..#.#..#.#..#.#....#..#.#....#....
+        ####..###..##...##..####..##..#....####.
+        "
+      `)
+    })
   })
 })
