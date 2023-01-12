34ef1059dc5602f105b43e4272b1edad70dd8b17
Cody Swartz <ctsstc@gmail.com>
2023-01-11 22:10:01 -0800

    Solve day 10 Part 1
    Co-authored-by: Lee Pham <LeeNPham@users.noreply.github.com>

--- src/day-10/day-10.mjs
+++ src/day-10/day-10.mjs
@@ -1,17 +1,107 @@
 import Problem from '../_lib/problem.mjs'
 
+const NOOP_COMMAND = 'noop'
+const ADDX_COMMAND = 'addx'
+const NOOP_CYCLE_COUNT = 1
+const ADDX_CYCLE_COUNT = 2
+
 class Day10 extends Problem {
+  #currentCycle = 0
+  #currentValue = 1
+  #sum = 0
+
   constructor(inputFileName) {
     super(inputFileName)
   }
 
   solvePart1() {
-    return this.lines.reduce((sb, line) => sb + line, '')
+    this.lines.forEach((line) => {
+      // console.log({line})
+      const [operation, strVal] = line.split(' ')
+
+      if (operation == NOOP_COMMAND) {
+        this.#currentCycle += NOOP_CYCLE_COUNT
+
+        this.#addSumIfSummableCycle()
+      }
+      else if (operation == ADDX_COMMAND) {
+        const value = parseInt(strVal)
+
+        for (let i = 0; i < ADDX_CYCLE_COUNT; i++) {
+          this.#currentCycle++
+
+          this.#addSumIfSummableCycle()
+
+          // Increase the current value on the second cycle
+          if (i == 1) {
+            this.#currentValue += value
+          }
+        }
+      }
+      else {
+        throw new Error("IMPOSSIBRUUUU!!!")
+      }
+
+    })
+
+    return this.#sum
   }
 
-  solvePart2() {
-    return parseInt(this.solvePart1())
+  #addSumIfSummableCycle() {
+    if (this.#isSummableCycle()) {
+      // console.log({
+      //   cycle: this.#currentCycle,
+      //   sum: this.#sum,
+      //   value: this.#currentValue,
+      //   signalStrength: this.#getSignalStrength()
+      // })
+      this.#sum += this.#getSignalStrength()
+    }
   }
+
+  #isSummableCycle() {
+    return this.#currentCycle == 20 || (this.#currentCycle - 20) % 40 == 0
+  }
+
+  #getSignalStrength() {
+    // console.log({
+    //   currentCycle: this.#currentCycle,
+    //   value: this.#currentValue,
+    //   signalStrength: this.#currentCycle * this.#currentValue
+    // })
+    return this.#currentCycle * this.#currentValue
+  }
+ 
+  /**
+   * Need to have an idea of a "cycle" counter, and current value / register
+   * 
+   * We have 2 operations: `addx` and `noop`
+   * 
+   * Any cycle increases the current cycle counter by 1
+   * 
+   * addx: consists of 2 cycles
+   *    1. the first cycle does nothing, the current value stays the same
+   *    2. the second cycle handles the addition to the current value
+   * 
+   * noop: consists of 1 cycle
+   *    1. does nothing
+   * 
+   * Signal Strength:
+   *  Is computed by taking the current cycle * the current value
+   * 
+   * Sum/Answer:
+   *  Every 20 + 40x cycles we need to add the current signal strength to a sum which will the final answer
+   * 
+   */
 }
 
+
+
+
+
+
+
+// solvePart2() {
+//   return parseInt(this.solvePart1())
+// }
 export default Day10
--- src/day-10/day-10.test.mjs
+++ src/day-10/day-10.test.mjs
@@ -8,9 +8,9 @@ const __dirname = dirname(__filename)
 describe('Day 10', () => {
   describe('example.txt', () => {
     const problem = new Problem(__dirname + '/example.txt')
-    // remember to take out xit and change it to it
-    xit('solves part 1', () => {
-      expect(problem.solvePart1()).toBe('1')
+
+    it('solves part 1', () => {
+      expect(problem.solvePart1()).toBe(13140)
     })
 
     // it('solves part 2', () => {
@@ -21,9 +21,9 @@ describe('Day 10', () => {
   describe('input.txt', () => {
     const problem = new Problem(__dirname + '/input.txt')
 
-    // it('solves part 1', () => {
-    //   expect(problem.solvePart1()).toBe('456789')
-    // })
+    it('solves part 1', () => {
+      expect(problem.solvePart1()).toBe(14040)
+    })
 
     // it('solves part 2', () => {
     //   expect(problem.solvePart2()).toBe(456789)
